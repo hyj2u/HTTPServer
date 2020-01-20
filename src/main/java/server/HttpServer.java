36618d2c98a5f9d27222fd5358e47fcbcaed710a
hyj2u <43366665+hyj2u@users.noreply.github.com>
2020-01-20 13:55:38 +0900

    Server Sockets

--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -3,6 +3,12 @@ package server;
 import config.Configuration;
 import config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 // Driver class for the Http Server
 public class HttpServer {
     public static void main(String[] args) {
@@ -14,6 +20,30 @@ public class HttpServer {
         System.out.println("Using Port: "+ conf.getPort());
         System.out.println("Using WebRoot: "+conf.getWebroot());
 
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            //read part
+            //write part
+            String html="<html><head><title>HTTPServer</title></head><body><h1>This is Simple HTTP Server served by Java</h1></body></html>";
+            final String CRLF="\n\r";
+            String response =
+                            "HTTP/1.1 200 OK"+CRLF+ //Status Line
+                            "Content-Length: "+html.getBytes().length+CRLF+ //header
+                            CRLF+
+                            html+
+                            CRLF+CRLF;
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
